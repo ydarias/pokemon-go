34e1c39d660fc6e7028ea179453cbe748a2bd809
Yeray Darias <ydarias@gmail.com>
2020-10-16 16:42:24 +0200

    Hello world of the REST API

---
+++ docs/DEV-NOTES.md
@@ -0,0 +1,9 @@
+```shell script
+go mod init github.com/ydarias/pokemon-go
+
+go get -u github.com/gin-gonic/gin
+```
+
+At this point the  dependencies are loaded and GoLand automatically detects using Vgo.
+
+`go mod download` to get all dependencies in local.
--- server/main.go
+++ server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ydarias/pokemon-go/server/controllers"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := gin.Default()
+
+	router.GET("/ping", controllers.Ping)
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
